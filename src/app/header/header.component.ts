c3bdfb3983cfef6215cbb5ddb2b02687d1e37de2
Matt Messer <51376954+mattMadeAThing@users.noreply.github.com>
2019-07-23 21:07:11 -0500

    Color pallet set, toolbar reworked + banner toggle

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Link } from '../Link';
 
 
 @Component({
@@ -7,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  // tslint:disable-next-line: max-line-length
+  readonly tabLinks: Link[] = [{path: 'about/postlist', name: 'about' } , {path: 'code/postlist', name: 'code'} , {path: 'music/postlist', name:'music'} , {path: 'writing/postlist', name:'writing'}];
+  banner: boolean = true;
   constructor() { }
 
   ngOnInit() {
   }
 
+  toggleBanner(){
+    this.banner = !this.banner;
+    console.log(this.banner);
+  }
 }
