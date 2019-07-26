df00cab7d0e335406a5efaf82a9ba945fa0c18bd
Matt Messer <51376954+mattMadeAThing@users.noreply.github.com>
2019-07-26 13:04:05 -0500

    adding about page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,14 +11,15 @@ import { PostDetailComponent } from './posts/post-detail/post-detail.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
 import { PostItemComponent } from './posts/post-list/post-item/post-item.component';
 import { PostService } from './posts/post.service';
-
+import { AboutComponent } from "./about/about.component";
 @NgModule({
   declarations: [
     AppComponent,
     PostDetailComponent,
     PostListComponent,
     PostItemComponent,
-    HeaderComponent
+    HeaderComponent,
+    AboutComponent
   ],
   imports: [
     MaterialModule,
