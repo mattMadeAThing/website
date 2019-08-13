af595d904d35733f467e2a334518a01c8e2f9829
Matt Messer <51376954+mattMadeAThing@users.noreply.github.com>
2019-08-12 19:38:23 -0500

    Adding comments, improving readability

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,10 @@ import { AboutComponent } from './about/about.component';
 const routes: Routes = [
   {path: 'about', component: AboutComponent},
   {path: ':category/postlist', component: PostListComponent},
-  {path: ':category/:id', component: PostDetailComponent}];
+  {path: ':category/:id', component: PostDetailComponent}
+  // :category and :id are sent as parameters to post.service.ts.
+  // They are used to define the JSON file for PostList and PostDetail and the specific post for PostDetail.
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes,
